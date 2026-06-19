302ea15e81701b7a1ec70a724d452156d36b7cb3
PutriNancin13 <putrinancin@gmail.com>
2026-06-20 00:49:00 +0800

    Update sistem

---
+++ app/Exports/PresensiHarianExport.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Presensi;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class PresensiHarianExport implements FromCollection, WithHeadings, WithMapping, WithColumnFormatting
+{
+    protected $tanggal;
+
+    public function __construct($tanggal)
+    {
+        $this->tanggal = $tanggal;
+    }
+
+    public function collection()
+    {
+        return Presensi::with(['user.tenagaKependidikan'])
+            ->whereDate('tanggal', $this->tanggal)
+            ->get();
+    }
+
+    public function map($p): array
+    {
+        return [
+            (string) ($p->user->tenagaKependidikan->nip ?? '-'),
+            $p->user->tenagaKependidikan->nama ?? '-',
+            $p->tanggal,
+            $p->jam_masuk ?? '-',
+            $p->jam_pulang ?? '-',
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'NIP',
+            'Nama Pegawai',
+            'Tanggal',
+            'Masuk',
+            'Pulang',
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_TEXT, // biar NIP tidak jadi E+17
+        ];
+    }
+}
